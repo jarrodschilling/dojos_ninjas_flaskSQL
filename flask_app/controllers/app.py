016d6c8af800215c98f67de2b4f58389bd19c191
jarrodschilling <jarrodschilling@gmail.com>
2023-11-12 09:14:03 -0700

    LEFT JOIN issues

--- flask_app/controllers/app.py
+++ flask_app/controllers/app.py
@@ -9,3 +9,7 @@ def show_all_dojos():
     dojos = Dojo.get_all()
     return render_template("dojos.html", all_dojos=dojos)
 
+@app.route('/dojos/<int:dojo_id>')
+def show_one_dojo(dojo_id):
+    dojo = Dojo.show_one_with_ninjas(dojo_id)
+    return render_template('show-dojo.html', dojo=dojo_id)
--- flask_app/models/dojo.py
+++ flask_app/models/dojo.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import ninja
 
 class Dojo:
     def __init__(self, data):
@@ -6,6 +7,7 @@ class Dojo:
         self.name = data['name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.ninjas = []
     
     @classmethod
     def get_all(cls):
@@ -17,8 +19,19 @@ class Dojo:
         return dojos
     
     @classmethod
-    def show_one(cls, dojo_id):
-        query = "SELECT * FROM dojos WHERE id = %(id)s;"
-        data = {'id': dojo_id}
+    def show_one_with_ninjas(cls, data):
+        query = "SELECT * FROM dojos LEFT JOIN ninjas ON ninjas.dojo_id = dojos.id WHERE dojos.id = %(id)s;"
         results = connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
-        return cls(results[0])
+        dojo = cls(results[0])
+
+        for row in results:
+            ninja_data = {
+                "id": row['ninjas.id'],
+                "first_name": row['first_name'],
+                "last_name": row['last_name'],
+                "age": row['age'],
+                "created_at": row['ninjas.created_at'],
+                "updated_at": row['ninjas.updated_at'],
+            }
+            dojo.ninjas.append(ninja.Ninja(ninja_data))
+        return dojo
